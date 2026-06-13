f1408dd2480dbc85ac9ea9c3ddf96b6c72074dd2
thom_101q <malinskitomek2@gmail.com>
2026-06-13 18:08:42 +0100

    Update: PV.py

--- PV.py
+++ PV.py
@@ -1,35 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
-def PV(I, T, Cashflow): #from Itertools
-    return Cashflow * ((1- I)**T)
+def PV(I, T, Cashflow): #PV Function discounting cash flows
+    return Cashflow / ((1 + I)**T)
 
 Cashflow = 1000
-I = [0.01, 0.03, 0.09]
-T = [1, 2, 3]
+I = [0.01, 0.03, 0.03, 0.05, 0.07]
+T = np.arange(1, (np.size(I)+1), 1)
+x = np.arange(0, (np.size(I)), 1)
+y = x
 
-CashGrid = np.ones((3, 3)) * Cashflow
+CashGrid = np.ones((np.size(I), np.size(T))) * Cashflow
 
-for x in range(1):
-    I = np.vstack((I, I, I))
-    T = np.vstack((T, T, T))
+I = np.tile(I, (np.size(I), 1))
+T = np.tile(T, (np.size(T), 1))
 
 I = np.rot90(I, k=3)
-
-x = [0, 1, 2]
-y = x
 
 for i in x:
     for j in y:
         CashGrid[i, j] = PV(I[i, j], T[i, j], Cashflow)
-        print(CashGrid[i, j])
+        # print(CashGrid[i, j])
 
 print(CashGrid)
 
+
+
+
+
 if __name__ == "__main__":
     print("Executed")
-
 
 
