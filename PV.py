31b9953303e3f02f01f4ccd32b0d4408fba61c52
thom_101q <malinskitomek2@gmail.com>
2026-06-13 18:35:32 +0100

    Updated: PV function with more parameters and flexibility

--- PV.py
+++ PV.py
@@ -4,19 +4,18 @@ import matplotlib.pyplot as plt
 def PV_equation(I, T, Cashflow): #PV Function discounting cash flows
     return Cashflow / ((1 + I)**T)
 
-def PV(Cashflow):
-    
-    I = [0.01, 0.02, 0.03, 0.05, 0.10]
+def PV(Cashflow, Start, End, Sep):
+
+    I = np.arange((Start / 100), (End / 100), (Sep / 100))
+    # I = [0.01, 0.02, 0.03, 0.04, 0.05]
     T = np.arange(1, (np.size(I)+1), 1)
     x = np.arange(0, (np.size(I)), 1)
     y = x
     
     CashGrid = np.ones((np.size(I), np.size(T))) * Cashflow
 
-    I = np.tile(I, (np.size(I), 1))
+    I = np.rot90(np.tile(I, (np.size(I), 1)), k=3) 
     T = np.tile(T, (np.size(T), 1))
-
-    I = np.rot90(I, k=3)
 
     for i in x:
         for j in y:
@@ -29,5 +28,6 @@ def PV(Cashflow):
 
 if __name__ == "__main__":
     print("Executed")
+
 
 
