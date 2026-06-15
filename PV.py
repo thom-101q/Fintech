81a8d0a1e7cb09edc1e9d2f731c0dc0d61f9d59f
thom_101q <malinskitomek2@gmail.com>
2026-06-15 11:27:59 +0100

    Change "PV.py"

--- PV.py
+++ PV.py
@@ -1,30 +1,42 @@
+#!/usr/bin/env python
+
+
+""" DOCSTRING HERE """
+
 import numpy as np
 import matplotlib.pyplot as plt
+
 
 def PV_equation(I, T, Cashflow): #PV Function discounting cash flows
     return Cashflow / ((1 + I)**T)
 
-def PV(Cashflow, Start, End, Sep):
 
-    I = np.arange((Start / 100), (End / 100), (Sep / 100))
-    # I = [0.01, 0.02, 0.03, 0.04, 0.05]
-    T = np.arange(1, (np.size(I)+1), 1)
-    x = np.arange(0, (np.size(I)), 1)
-    y = x
     
-    CashGrid = np.ones((np.size(I), np.size(T))) * Cashflow
+    
+    
+def PV(Cashflow, Start, End, Sep):
+    
+    I = np.arange((Start / 100), (End / 100), (Sep / 100))
+    I_size = np.size(I)
+    I = np.rot90(np.tile(I, (I_size, 1)), k=3)
+    
+    T = np.arange(1, (I_size+1), 1)
+    T_size = np.size(T)
+    T = np.tile(T, (T_size, 1))
+    
+    x = np.arange(0, (I_size), 1)
+    y = np.arange(0, (T_size), 1)
 
-    I = np.rot90(np.tile(I, (np.size(I), 1)), k=3) 
-    T = np.tile(T, (np.size(T), 1))
-
+    CashGrid = np.ones((I_size, T_size)) * Cashflow
+    
     for i in x:
         for j in y:
-            CashGrid[i, j] = PV_equation(I[i, j], T[i, j], Cashflow)
-            # print(CashGrid[i, j])
+            CashGrid[i][j] = PV_equation(I[i][j], T[i][j], Cashflow)
+            print(CashGrid[i, j])
 
     print(CashGrid)
 
-
+PV(1000, 0, 10, 1)
 
 if __name__ == "__main__":
     print("Executed")
