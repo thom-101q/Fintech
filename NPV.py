84be3aae345e80467381b66e820ff44ec857f833
thom <malinskitomek2@gmail.com>
2026-06-23 20:31:14 +0100

    Updated: NPV.py:

--- NPV.py
+++ NPV.py
@@ -15,11 +15,28 @@ def PV(Rate, Time, FutureCash):
 
 # Costs = Function(cash)
 # Benefits = Function(FutureCash)
+def Benefits(B_Cashflow, B_Rate):
+    # any function Benefits
+    # develop way to imput the benefit of any project
+    # I'm gonna start with basic stream of compounding cash flows
+    
+    return B_Cashflow * (1 + B_Rate)**Time
 
-Benefits = 100
-Costs = 10
-Time = 0
+def Costs(Cost):
+    #Want a cost function just like benefits
+    #For now we're gonna say its a fixed 3 year cost of x cash each
+    return Cost * np.linspace(1, 1, 3)
+    
+
+Costs = Costs(5000)
+Time = np.arange(0, 10, 1)
 Rate = 0.02
+
+
+
+Benefits = Benefits(1000, 0.08)
+
+K_Time, K_Costs, K_Benefits = np.meshgrid(Time, Costs, Benefits, indexing="ij")
 
 
 # NPV = PV(Benefits) - PV(Costs)
