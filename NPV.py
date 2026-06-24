254669a18132545e896aecf6b4004b1d694c74d2
thom <malinskitomek2@gmail.com>
2026-06-24 14:46:46 +0100

    Saving Work on NPV.py

--- NPV.py
+++ NPV.py
@@ -11,10 +11,10 @@ This script calculates the net present value of a financial deal using numpy.
 """
 import numpy as np
 import matplotlib.pyplot as plt
-7
-def PV(Rate, Time, FutureCash):
-    return FutureCash / ((1 + Rate)**Time)
 
+def PV(Rate, Time, FutureCash):
+    
+    return (FutureCash / ((1 + Rate)**Time))
 
 # Costs = Function(cash)
 # Benefits = Function(FutureCash)
@@ -25,34 +25,42 @@ def Benefits(B_Cashflow, B_Rate):
     
     return B_Cashflow * (1 + B_Rate)**Time
 
-def Costs(Cost):
+def Costs(Cost, Year):
     #Want a cost function just like benefits
-    #For now we're gonna say its a fixed 3 year cost of x cash each#
-    empty = np.zeros(10)
-    Cost = Cost * np.linspace(1, 1, 3)
-    for Cost[0:3] in range(3):
-        empty[0:3] = empty[0:3] + Cost[0:3]
-        
-    return Cost
+    #For now we're gonna say its a fixed x year cost of x cash each#
+    empty = np.zeros(10 - Year)
+    Cost = Cost * np.linspace(1, 1, Year)
+    Cost = np.append(Cost, empty)
     
+    return Cost
 
-Costs = Costs(5000)
+
+
+Costs = Costs(5000, 3)
 Time = np.arange(0, 10, 1)
 
 Rate = 0.02
 
-
-
 Benefits = Benefits(1000, 0.08)
 
 #np.meshgrid does not work for three variables
-K_Time, K_Costs = np.meshgrid(Time, Costs, indexing="ij")
+# K_Time, K_Costs = np.meshgrid(Time, Costs, indexing="ij")
 
 
-# NPV = PV(Benefits) - PV(Costs)
-Var = PV(Rate, Time, Benefits) - PV(Rate, Time, Costs)
+# This needs to go inside the final function for NPV
+for x in range(np.size(Time)):
+    Benefits[x] = PV(0.02, Time[x], Benefits[x])
+    Costs[x] = PV(0.02, Time[x], Costs[x])
+    print(Benefits[x])
 
-print(Var)
+def NPV():
+
+    NPV = np.sum(Benefts) - np.sum(Costs)
+
+# # NPV = PV(Benefits) - PV(Costs)
+# Var = PV(Rate, Time, Benefits) - PV(Rate, Time, Costs)
+
+# print(Var)
 
 
 
