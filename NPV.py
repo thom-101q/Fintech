fc2e4b18615373e76673b2067cb4157f1b373c44
thom <malinskitomek2@gmail.com>
2026-06-24 10:51:49 +0100

    Update to NPV, Inflation. 	Work Done on NPV Protocols and equations more nonesense but it was fine 	Became more based lol 	Feel good, ego, better!

--- NPV.py
+++ NPV.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 """ 
+This script calculates the net present value of a financial deal using numpy.
+
+# The goal is to make a interperatable function with easily adjusted parameters.
 
 
 
@@ -8,7 +11,7 @@
 """
 import numpy as np
 import matplotlib.pyplot as plt
-
+7
 def PV(Rate, Time, FutureCash):
     return FutureCash / ((1 + Rate)**Time)
 
@@ -24,19 +27,26 @@ def Benefits(B_Cashflow, B_Rate):
 
 def Costs(Cost):
     #Want a cost function just like benefits
-    #For now we're gonna say its a fixed 3 year cost of x cash each
-    return Cost * np.linspace(1, 1, 3)
+    #For now we're gonna say its a fixed 3 year cost of x cash each#
+    empty = np.zeros(10)
+    Cost = Cost * np.linspace(1, 1, 3)
+    for Cost[0:3] in range(3):
+        empty[0:3] = empty[0:3] + Cost[0:3]
+        
+    return Cost
     
 
 Costs = Costs(5000)
 Time = np.arange(0, 10, 1)
+
 Rate = 0.02
 
 
 
 Benefits = Benefits(1000, 0.08)
 
-K_Time, K_Costs, K_Benefits = np.meshgrid(Time, Costs, Benefits, indexing="ij")
+#np.meshgrid does not work for three variables
+K_Time, K_Costs = np.meshgrid(Time, Costs, indexing="ij")
 
 
 # NPV = PV(Benefits) - PV(Costs)
